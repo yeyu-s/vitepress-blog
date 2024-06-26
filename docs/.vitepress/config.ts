db7b0e50747a9784371ab7f132c18dd9b29922ec
guosen <guosen@delilegal.com>
2024-06-26 18:53:31 +0800

    指定pnpm版本

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vitepress-blog/',
   title: "my test blog",
   description: "a vitepress project",
   themeConfig: {
